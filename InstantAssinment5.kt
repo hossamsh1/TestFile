d2b1594e217842298e7eb011a043c149a5ff7d33
HOSSAM shaaban <159369855+hossamsh1@users.noreply.github.com>
2024-08-11 17:23:21 +0300

    Add files via upload

---
+++ InstantAssinment5.kt
@@ -0,0 +1,15 @@
+package com.example.asinmentinstant5
+
+import androidx.compose.runtime.Composable
+import org.w3c.dom.Text
+import java.lang.reflect.Modifier
+
+
+@Composable
+fun Instant(modifier: Modifier){
+    println("Hello world")
+}
+
+fun main() {
+    println("Hello World")
+}
